57bb0127aeec1e6408bb3877fc894a23188757e3
mehwish0711 <mehwishweb123@gmail.com>
2025-06-26 22:21:21 +0500

    add order tracking li

--- resources/views/admin-dashboard/layout/apps.blade.php
+++ resources/views/admin-dashboard/layout/apps.blade.php
@@ -63,7 +63,7 @@
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" data-toggle="collapse" href="#ui-basics" aria-expanded="false" aria-controls="ui-basic">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basics" aria-expanded="false" aria-controls="ui-basics">
               <i class="icon-layout menu-icon"></i>
               <span class="menu-title">Category</span>
               <i class="menu-arrow"></i>
@@ -76,11 +76,27 @@
         
               </ul>
             </div>
+            
+          </li>
+            <li class="nav-item ">
+            <a class="nav-link" data-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+              <i class="icon-layout menu-icon"></i>
+              <span class="menu-title">Track Orders</span>
+              <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+              <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="{{route('product')}}">Orders List</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{route('add-products')}}">Order Items</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{route('add-products')}}">Customers</a></li>
+               </ul>
+            </div>
           </li>
           
          
       
         </ul>
+        
       </nav>
       <!-- partial -->
      @yield('content')
